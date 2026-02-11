d40f736a6b5c3c77f103b966f4ee33173f8f1a7e
Ohm Patel <ohmp3@usf.edu>
2026-02-11 14:23:33 -0500

    Created Array of Objects

---
+++ cc_4.js
@@ -0,0 +1,8 @@
+// Step 2: Creating an Array
+
+let products = [
+    {Name: "Chocolate Brownie", Category: "Sweets", Price: 2.99, Inventory: 50}
+    {Name: "Water", Category: "Drinks", Price: 1.99, Inventory: 150}
+    {Name: "Double Chocolate Chunk Brownie", Category: "Sweets", Price: 3.99, Inventory: 80}
+    {Name: "I Love Brownies Shirt", Category: "Apparel", Price: 15.99, Inventory: 100}
+    {Name: "Parchment Paper", Category: "Miscellaneous", Price: 10.99, Inventory: 200}
